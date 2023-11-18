7d7d5764a53bfe47a85bb99f6a52c15bbb412857
sohanr <mizan.rahman66d@gmail.com>
2023-11-19 01:31:05 +0600

    work item

---
+++ src/components/projects/WorkItem.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FaArrowRightLong } from "react-icons/fa6";
+
+const WorkItem = ({item}) => {
+  const techList = () =>{
+    
+     const excludedTags = ['Full Stack', 'Frontend'];
+
+     const shouldExclude = excludedTags.some(tag => item.tag.includes(tag));
+
+     const list = shouldExclude ? item.tag.filter(tag => !excludedTags.includes(tag)) : item.tag;
+
+     return list.join(', ')
+  }
+  return (
+    <div className='work_card'>
+        
+        <img src={item.image} alt="demo" className='work_img'/>
+        <h3 className='work_title'>{item.title}</h3>
+        <p className='work_details'>{item.details}</p>
+        <p className='work_tags'>Tech: {techList()}</p>
+        
+        <div className='work_buttons'>
+            <a href={item.live} className='work_button'>
+                Demo <FaArrowRightLong  className='work_button-icon'/>
+            </a>
+            <a href={item.code} className='work_button'>
+                Code <FaArrowRightLong  className='work_button-icon'/>
+            </a>            
+        </div>
+        
+    </div>
+  )
+}
+
+export default WorkItem
