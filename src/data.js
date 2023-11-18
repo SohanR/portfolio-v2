29104b68ff91738ec77e580b2029c5c3966d384c
sohanr <mizan.rahman66d@gmail.com>
2023-11-19 01:31:57 +0600

    portfolio data

---
+++ src/data.js
@@ -0,0 +1,128 @@
+import work1 from './assets/work1.jpg'
+
+export const profile =  {
+        "name": "Md. Mizanur Rahman",
+        "subtitle":"Software Engineer",
+        "description":" test description",
+        "github":"https://github.com/SohanR",
+        "linkedin":"https://www.linkedin.com/in/mizanurrahman/",
+    }
+
+
+export const about = {
+    "title":"About Me",
+    "subtitle":"My introduction",
+    "description":"test description",
+}
+
+export const skills ={
+    "title":"Skills",
+    "subtitle":"My Technical Level",
+    "frontend":{
+        "title":"Frontend Developer",
+        "skills":["React","Redux","React Native","Next JS"]
+    },
+    "backend":{
+        "title":"Backend Developer",
+        "skills":["Node.js","Express.js","Nest.js","MongoDB"]
+    }
+}
+
+export const experience = {
+    "title":"Experience",
+    "subtitle":"My Journey",
+    "data":[
+        {
+            "name":"Zaynax Health",
+            "role":"Backend Developer Intern",
+            "date":"April, 2023 - Aug, 2023"
+        },
+        {
+            "name":"Absolute Zero LTD",
+            "role":"Shopify Developer (Remote)",
+            "date":"April, 2023 - Aug, 2023"
+        }
+    ]
+}
+
+
+
+
+export const projectData = [
+    {
+        "title": "Book My Hotel",
+        "details":"Created a Hotel Booking Platform where users can book rooms and sell their own hotels. One user can be both a buyer and a seller at the same time. Made it easy to use and secure for everyone",
+        "image":work1,
+        "live":"link ",
+        "code":"link ",
+        "tag":['Full Stack','ReactJS','NodeJS','ExpressJS','MongoDB'],
+    },
+    // {
+    //     "title": "project 1",
+    //     "details":"test",
+    //     "image": work2,
+    //     "live":"link ",
+    //     "code":"link ",
+    //     "tag":[1,2,3,4,5,6],
+    // },
+    // {
+    //     "title": "project 1",
+    //     "details":"test",
+    //     "image": work3,
+    //     "live":"link ",
+    //     "code":"link ",
+    //     "tag":[1,2,3,4,5,6],
+    // },
+    // {
+    //     "title": "project 1",
+    //     "details":"test",
+    //     "image":work4,
+    //     "live":"link ",
+    //     "code":"link ",
+    //     "tag":[1,2,3,4,5,6],
+    // },
+    // {
+    //     "title": "project 1",
+    //     "details":"test",
+    //     "image":work5,
+    //     "live":"link ",
+    //     "code":"link ",
+    //     "tag":[1,2,3,4,5,6],
+    // }
+]
+
+
+
+export const projectNav = [
+    {
+        "name": 'all'
+    },
+
+    {
+        "name": 'Full Stack'
+    },
+
+    {
+        "name": 'Frontend'
+    },
+
+    {
+        "name": 'ReactJS'
+    },
+    
+    {
+        "name": 'NextJS'
+    },    
+    
+    {
+        "name": 'NestJS'
+    },
+
+    {
+        "name": 'ExpressJS'
+    },
+
+    {
+        "name": 'React Native'
+    },
+]
