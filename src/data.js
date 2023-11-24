149899036f2e65c44e8665260b42b08a2f0d3d40
sohanr <mizan.rahman66d@gmail.com>
2023-11-25 01:33:45 +0600

    data updated

--- src/data.js
+++ src/data.js
@@ -1,5 +1,16 @@
 import work1 from  './assets/work1.png'
 import work2 from  './assets/work2.png'
+import work3 from  './assets/work3.png'
+import work4 from  './assets/work4.png'
+import work5 from  './assets/work5.png'
+import work6 from  './assets/work6.png'
+import work7 from  './assets/work7.png'
+import work8 from  './assets/work8.png'
+import work9 from  './assets/work9.png'
+import work10 from  './assets/work10.png'
+import work11 from  './assets/work11.png'
+import work12 from  './assets/work12.png'
+
 
 
 export const profile =  {
@@ -67,6 +78,92 @@ export const projectData = [
         "code":"https://github.com/SohanR/send-email-using-react",
         "tag":['Frontend','ReactJS','EmailJS'],
     },
+    {
+        "title": "Drag and Drop Image gallery",
+        "details":"This is a small image gallery app with an intuitive drag-and-drop feature",
+        "image":work3,
+        "live":"https://ollyo-task-gallery.netlify.app/",
+        "code":"https://github.com/SohanR/image-gallery",
+        "tag":['Frontend','ReactJS','dnd-kit','React Drag n Drop'],
+    },
+    {
+        "title": "Link To QR Code Generator",
+        "details":"Input any URL to generate a QR Code with the feature of changing the size of the generated QR Code.",
+        "image":work4,
+        "live":"https://linktoqrcode.netlify.app/",
+        "code":"https://github.com/SohanR/linkToQrcode",
+        "tag":['Frontend','ReactJS','react.qrcode'],
+    },
+    {
+        "title": "Private chat room",
+        "details":"A private chat room using socket-io.",
+        "image":work5,
+        "live":"https://private-chat-rooms.netlify.app/",
+        "code":"https://github.com/SohanR/private-chat",
+        "tag":['Full Stack','ReactJS','ExpressJS','NodeJS','Socket.io'],
+    },
+    {
+        "title": "PinkFLoyd VSCode Theme",
+        "details":"Pink FLoyd inspired color theme.",
+        "image":work6,
+        "live":"https://marketplace.visualstudio.com/items?itemName=sohanr.pinkfloyd-theme",
+        "code":"https://github.com/SohanR/PinkFloyd-vscode-theme",
+        "tag":['Misc'],
+    },
+    {
+        "title": "Face Expression Recognition",
+        "details":"Face Expression Recognition app build using tensorflow js. It detects face and draw landmarks, then recognize expression.",
+        "image":work7,
+        "live":"https://github.com/SohanR/Face-Expression-Recognition",
+        "code":"https://github.com/SohanR/Face-Expression-Recognition",
+        "tag":['Misc', 'TensorflowJS', 'Face-ApiJS'],
+    },
+    {
+        "title": "Leetcode 100 DSA Problems & Solutions",
+        "details":" I have curated a list of the top 100 DSA problems from LeetCode, covering a wide array of topics such as Array, String, Linked List, Graph, Dynamic Programming, Tree, Stack and Queue.",
+        "image":work9,
+        "live":"https://github.com/SohanR/100-DSA-Interview-Problems",
+        "code":"https://github.com/SohanR/100-DSA-Interview-Problems",
+        "tag":['Leetcode'],
+    },
+    {
+        "title": "Leetcode Problem & Solution",
+        "details":"My all leetcode solution organizing by tags,company and more",
+        "image":work10,
+        "live":"https://github.com/SohanR/LeetCode-solution",
+        "code":"https://github.com/SohanR/LeetCode-solution",
+        "tag":['Leetcode'],
+    },
+        {
+        "title": "Neetcode 150 Problem & Solution",
+        "details":"Problems & Solutions of all 150 leetcode problem that are listed on Neetcode.io",
+        "image":work11,
+        "live":"https://github.com/SohanR/Neetcode-150",
+        "code":"https://github.com/SohanR/Neetcode-150",
+        "tag":['Leetcode'],
+    },
+    
+    
+    
+    
+    
+    
+    {
+        "title": "Lyrics Search",
+        "details":"Search any song's lyrics by song name,album name, artist name",
+        "image":work8,
+        "live":"https://sohanr.github.io/lyric-search/",
+        "code":"https://github.com/SohanR/lyric-search",
+        "tag":['VanilaJS'],
+    },
+    {
+        "title": "Meal Search",
+        "details":"Meal search is a apps for searching recipe of meal and it also provide meal's images.",
+        "image":work12,
+        "live":"https://sohanr.github.io/meal-search/",
+        "code":"https://github.com/SohanR/meal-search",
+        "tag":['VanilaJS'],
+    },
 ]
 
 
@@ -102,6 +199,15 @@ export const projectNav = [
 
     {
         "name": 'React Native'
+    },
+    {
+        "name": 'VanilaJS'
+    },
+    {
+        "name": 'Leetcode'
+    },
+    {
+        "name": 'Misc'
     },
 ]
 
