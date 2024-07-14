0c4071bba839765928719875606f8a103075ce09
sohanr <mizan.rahman66d@gmail.com>
2024-07-14 23:47:02 +0600

    data update

--- src/data.js
+++ src/data.js
@@ -1,279 +1,288 @@
-import work1 from  './assets/work1.png'
-import work2 from  './assets/work2.png'
-import work3 from  './assets/work3.png'
-import work4 from  './assets/work4.png'
-import work5 from  './assets/work5.png'
-import work6 from  './assets/work6.png'
-import work7 from  './assets/work7.png'
-import work8 from  './assets/work8.png'
-import work9 from  './assets/work9.png'
-import work10 from  './assets/work10.png'
-import work11 from  './assets/work11.png'
-import work12 from  './assets/work12.png'
-import work13 from  './assets/work13.png'
-import work14 from  './assets/work14.png'
-import work15 from  './assets/work15.png'
-import work16 from  './assets/work16.png'
+import work1 from "./assets/work1.png";
+import work2 from "./assets/work2.png";
+import work3 from "./assets/work3.png";
+import work4 from "./assets/work4.png";
+import work5 from "./assets/work5.png";
+import work6 from "./assets/work6.png";
+import work7 from "./assets/work7.png";
+import work8 from "./assets/work8.png";
+import work9 from "./assets/work9.png";
+import work10 from "./assets/work10.png";
+import work11 from "./assets/work11.png";
+import work12 from "./assets/work12.png";
+import work13 from "./assets/work13.png";
+import work14 from "./assets/work14.png";
+import work15 from "./assets/work15.png";
+import work16 from "./assets/work16.png";
 
-
-
-
-export const profile =  {
-        "name": "Mizanur Rahman Sohan",
-        "subtitle":"Software Engineer",
-        "description":"JavaScript enthusiast and software engineer dedicated to crafting flawless digital experiences. Building pixel-perfect solutions for a connected world, one line of code at a time. ",
-        "github":"https://github.com/SohanR",
-        "linkedin":"https://www.linkedin.com/in/mizanurrahman/",
-    }
-
+export const profile = {
+  name: "Mizanur Rahman Sohan",
+  subtitle: "Software Engineer",
+  description:
+    "JavaScript enthusiast and software engineer dedicated to crafting flawless digital experiences. Building pixel-perfect solutions for a connected world, one line of code at a time. ",
+  github: "https://github.com/SohanR",
+  linkedin: "https://www.linkedin.com/in/mizanurrahman/",
+};
 
 export const about = {
-    "title":"About Me",
-    "subtitle":"My introduction",
-    "description":"Seasoned Full-Stack Developer proficient in MERN stack and JavaScript technologies, with expertise in TypeScript, MongoDB queries, Nest.js, and Express.js. Experienced in crafting robust web applications, specializing in microservices architecture. Agile-minded, detail-oriented, and driven by a passion for problem-solving, continuously adapting to embrace new technologies for ongoing growth.",
-}
+  title: "About Me",
+  subtitle: "My introduction",
+  description:
+    "Seasoned Full-Stack Developer proficient in MERN stack and JavaScript technologies, with expertise in TypeScript, MongoDB queries, Nest.js, and Express.js. Experienced in crafting robust web applications, specializing in microservices architecture. Agile-minded, detail-oriented, and driven by a passion for problem-solving, continuously adapting to embrace new technologies for ongoing growth.",
+};
 
-export const skills ={
-    "title":"Skills",
-    "subtitle":"My Technical Level",
-    "frontend":{
-        "title":"Frontend",
-        "skills":["React","Redux","React Native","Next JS"]
-    },
-    "backend":{
-        "title":"Backend",
-        "skills":["Node.js","Express.js","Nest.js","MongoDB"]
-    }
-}
+export const skills = {
+  title: "Skills",
+  subtitle: "My Technical Level",
+  frontend: {
+    title: "Frontend",
+    skills: ["React", "Redux", "React Native", "Next JS"],
+  },
+  backend: {
+    title: "Backend",
+    skills: ["Node.js", "Express.js", "Nest.js", "MongoDB"],
+  },
+};
 
 export const experience = {
-    "title":"Experience",
-    "subtitle":"My Journey",
-    "data":[
-        {
-            "name":"Zaynax Health",
-            "role":"Backend Developer",
-            "date":"April, 2023 - Aug, 2023"
-        },
-        {
-            "name":"Absolute Zero LTD",
-            "role":"Shopify Developer (Remote)",
-            "date":"March, 2021 - Dec, 2022"
-        }
-    ]
-}
-
-
-
+  title: "Experience",
+  subtitle: "My Journey",
+  data: [
+    {
+      name: "K53 Tech Solution",
+      role: "Software Engineer",
+      date: "July, 2024 - Present",
+    },
+    {
+      name: "Dotpot iT",
+      role: "Full Stack Software Engineer",
+      date: "December, 2023 - june, 2024",
+    },
+    {
+      name: "Zaynax Health",
+      role: "Backend Developer",
+      date: "April, 2023 - November, 2023",
+    },
+    {
+      name: "Absolute Zero LTD",
+      role: "Shopify Developer (Remote)",
+      date: "March, 2021 - Dec, 2022",
+    },
+  ],
+};
 
 export const projectData = [
-    {
-        "title": "Book My Hotel",
-        "details":"Created a Hotel Booking Platform where users can book rooms and sell their own hotels. One user can be both a buyer and a seller at the same time. Made it easy to use and secure for everyone",
-        "image":work1,
-        "live":"https://bookmyhotels.netlify.app/ ",
-        "code":"https://github.com/SohanR/book-my-hotel/ ",
-        "tag":['Full Stack','ReactJS','NodeJS','ExpressJS','MongoDB'],
-    },
-    {
-        "title": "Send Email Using React",
-        "details":"This project allows sending emails directly from the client-side using the EmailJS library, eliminating the need for a backend service like Node.js or a mail server setup.",
-        "image":work2,
-        "live":"https://react-send-email.netlify.app/",
-        "code":"https://github.com/SohanR/send-email-using-react",
-        "tag":['Frontend','ReactJS','EmailJS'],
-    },
-    {
-        "title": "Drag and Drop Image gallery",
-        "details":"This is a small image gallery app with an intuitive drag-and-drop feature",
-        "image":work3,
-        "live":"https://ollyo-task-gallery.netlify.app/",
-        "code":"https://github.com/SohanR/image-gallery",
-        "tag":['Frontend','ReactJS','dnd-kit','React Drag n Drop'],
-    },
-    {
-        "title": "Link To QR Code Generator",
-        "details":"Input any URL to generate a QR Code with the feature of changing the size of the generated QR Code.",
-        "image":work4,
-        "live":"https://linktoqrcode.netlify.app/",
-        "code":"https://github.com/SohanR/linkToQrcode",
-        "tag":['Frontend','ReactJS','react.qrcode'],
-    },
-    {
-        "title": "Private chat room",
-        "details":"A private chat room using socket-io.",
-        "image":work5,
-        "live":"https://private-chat-rooms.netlify.app/",
-        "code":"https://github.com/SohanR/private-chat",
-        "tag":['Full Stack','ReactJS','ExpressJS','NodeJS','Socket.io'],
-    },
-    {
-        "title": "PinkFLoyd VSCode Theme",
-        "details":"Pink FLoyd inspired color theme.",
-        "image":work6,
-        "live":"https://marketplace.visualstudio.com/items?itemName=sohanr.pinkfloyd-theme",
-        "code":"https://github.com/SohanR/PinkFloyd-vscode-theme",
-        "tag":['Misc'],
-    },
-    {
-        "title": "Face Expression Recognition",
-        "details":"Face Expression Recognition app build using tensorflow js. It detects face and draw landmarks, then recognize expression.",
-        "image":work7,
-        "live":"https://github.com/SohanR/Face-Expression-Recognition",
-        "code":"https://github.com/SohanR/Face-Expression-Recognition",
-        "tag":['Misc', 'TensorflowJS', 'Face-ApiJS'],
-    },
-    {
-        "title": "Leetcode 100 DSA Problems & Solutions",
-        "details":" I have curated a list of the top 100 DSA problems from LeetCode, covering a wide array of topics such as Array, String, Linked List, Graph, Dynamic Programming, Tree, Stack and Queue.",
-        "image":work9,
-        "live":"https://github.com/SohanR/100-DSA-Interview-Problems",
-        "code":"https://github.com/SohanR/100-DSA-Interview-Problems",
-        "tag":['Leetcode'],
-    },
-    {
-        "title": "Leetcode Problem & Solution",
-        "details":"My all leetcode solution organizing by tags,company and more",
-        "image":work10,
-        "live":"https://github.com/SohanR/LeetCode-solution",
-        "code":"https://github.com/SohanR/LeetCode-solution",
-        "tag":['Leetcode'],
-    },
-        {
-        "title": "Neetcode 150 Problem & Solution",
-        "details":"Problems & Solutions of all 150 leetcode problem that are listed on Neetcode.io",
-        "image":work11,
-        "live":"https://github.com/SohanR/Neetcode-150",
-        "code":"https://github.com/SohanR/Neetcode-150",
-        "tag":['Leetcode'],
-    },
-    
-    
-    
-    
-    
-    
-    {
-        "title": "Lyrics Search",
-        "details":"Search any song's lyrics by song name,album name, artist name",
-        "image":work8,
-        "live":"https://sohanr.github.io/lyric-search/",
-        "code":"https://github.com/SohanR/lyric-search",
-        "tag":['VanillaJS'],
-    },
-    {
-        "title": "Meal Search",
-        "details":"Meal search is a apps for searching recipe of meal and it also provide meal's images.",
-        "image":work12,
-        "live":"https://sohanr.github.io/meal-search/",
-        "code":"https://github.com/SohanR/meal-search",
-        "tag":['VanillaJS'],
-    },
-    {
-        "title": "Currency Converter",
-        "details":"A simple Currency Converter. It will give you exchange rate from one currency to onether currency..",
-        "image":work15,
-        "live":"https://sohanr.github.io/Currency-Converter/",
-        "code":"https://github.com/SohanR/Currency-Converter",
-        "tag":['VanillaJS'],
-    },
-    {
-        "title": "Drum Kit",
-        "details":"The project allows users to simulate playing drums by pressing corresponding keys, showcasing proficiency in front-end development and user interface design.",
-        "image":work13,
-        "live":"https://sohanr.github.io/Drum-kit/",
-        "code":"https://github.com/SohanR/Drum-kit",
-        "tag":['VanillaJS'],
-    },
-    {
-        "title": "Type Fast",
-        "details":"typing mini-game utilizing Vanilla JavaScript, where players are challenged to swiftly type displayed words for scores and time extension. Implemented features include difficulty level selection, dynamic time extension upon accurate word typing, and a game-over scenario upon time expiration.",
-        "image":work14,
-        "live":"https://sohanr.github.io/type-fast/",
-        "code":"https://github.com/SohanR/type-fast",
-        "tag":['VanillaJS'],
-    },
-    {
-        "title": "To Do",
-        "details":"A simple to do app with simple animation effect with feature of filtering, mark as done and deleting.",
-        "image":work16,
-        "live":"https://github.com/SohanR/todo-list",
-        "code":"https://sohanr.github.io/todo-list/",
-        "tag":['VanillaJS'],
-    },
-    
-]
+  {
+    title: "Book My Hotel",
+    details:
+      "Created a Hotel Booking Platform where users can book rooms and sell their own hotels. One user can be both a buyer and a seller at the same time. Made it easy to use and secure for everyone",
+    image: work1,
+    live: "https://bookmyhotels.netlify.app/ ",
+    code: "https://github.com/SohanR/book-my-hotel/ ",
+    tag: ["Full Stack", "ReactJS", "NodeJS", "ExpressJS", "MongoDB"],
+  },
+  {
+    title: "Send Email Using React",
+    details:
+      "This project allows sending emails directly from the client-side using the EmailJS library, eliminating the need for a backend service like Node.js or a mail server setup.",
+    image: work2,
+    live: "https://react-send-email.netlify.app/",
+    code: "https://github.com/SohanR/send-email-using-react",
+    tag: ["Frontend", "ReactJS", "EmailJS"],
+  },
+  {
+    title: "Drag and Drop Image gallery",
+    details:
+      "This is a small image gallery app with an intuitive drag-and-drop feature",
+    image: work3,
+    live: "https://ollyo-task-gallery.netlify.app/",
+    code: "https://github.com/SohanR/image-gallery",
+    tag: ["Frontend", "ReactJS", "dnd-kit", "React Drag n Drop"],
+  },
+  {
+    title: "Link To QR Code Generator",
+    details:
+      "Input any URL to generate a QR Code with the feature of changing the size of the generated QR Code.",
+    image: work4,
+    live: "https://linktoqrcode.netlify.app/",
+    code: "https://github.com/SohanR/linkToQrcode",
+    tag: ["Frontend", "ReactJS", "react.qrcode"],
+  },
+  {
+    title: "Private chat room",
+    details: "A private chat room using socket-io.",
+    image: work5,
+    live: "https://private-chat-rooms.netlify.app/",
+    code: "https://github.com/SohanR/private-chat",
+    tag: ["Full Stack", "ReactJS", "ExpressJS", "NodeJS", "Socket.io"],
+  },
+  {
+    title: "PinkFLoyd VSCode Theme",
+    details: "Pink FLoyd inspired color theme.",
+    image: work6,
+    live: "https://marketplace.visualstudio.com/items?itemName=sohanr.pinkfloyd-theme",
+    code: "https://github.com/SohanR/PinkFloyd-vscode-theme",
+    tag: ["Misc"],
+  },
+  {
+    title: "Face Expression Recognition",
+    details:
+      "Face Expression Recognition app build using tensorflow js. It detects face and draw landmarks, then recognize expression.",
+    image: work7,
+    live: "https://github.com/SohanR/Face-Expression-Recognition",
+    code: "https://github.com/SohanR/Face-Expression-Recognition",
+    tag: ["Misc", "TensorflowJS", "Face-ApiJS"],
+  },
+  {
+    title: "Leetcode 100 DSA Problems & Solutions",
+    details:
+      " I have curated a list of the top 100 DSA problems from LeetCode, covering a wide array of topics such as Array, String, Linked List, Graph, Dynamic Programming, Tree, Stack and Queue.",
+    image: work9,
+    live: "https://github.com/SohanR/100-DSA-Interview-Problems",
+    code: "https://github.com/SohanR/100-DSA-Interview-Problems",
+    tag: ["Leetcode"],
+  },
+  {
+    title: "Leetcode Problem & Solution",
+    details: "My all leetcode solution organizing by tags,company and more",
+    image: work10,
+    live: "https://github.com/SohanR/LeetCode-solution",
+    code: "https://github.com/SohanR/LeetCode-solution",
+    tag: ["Leetcode"],
+  },
+  {
+    title: "Neetcode 150 Problem & Solution",
+    details:
+      "Problems & Solutions of all 150 leetcode problem that are listed on Neetcode.io",
+    image: work11,
+    live: "https://github.com/SohanR/Neetcode-150",
+    code: "https://github.com/SohanR/Neetcode-150",
+    tag: ["Leetcode"],
+  },
 
-
+  {
+    title: "Lyrics Search",
+    details: "Search any song's lyrics by song name,album name, artist name",
+    image: work8,
+    live: "https://sohanr.github.io/lyric-search/",
+    code: "https://github.com/SohanR/lyric-search",
+    tag: ["VanillaJS"],
+  },
+  {
+    title: "Meal Search",
+    details:
+      "Meal search is a apps for searching recipe of meal and it also provide meal's images.",
+    image: work12,
+    live: "https://sohanr.github.io/meal-search/",
+    code: "https://github.com/SohanR/meal-search",
+    tag: ["VanillaJS"],
+  },
+  {
+    title: "Currency Converter",
+    details:
+      "A simple Currency Converter. It will give you exchange rate from one currency to onether currency..",
+    image: work15,
+    live: "https://sohanr.github.io/Currency-Converter/",
+    code: "https://github.com/SohanR/Currency-Converter",
+    tag: ["VanillaJS"],
+  },
+  {
+    title: "Drum Kit",
+    details:
+      "The project allows users to simulate playing drums by pressing corresponding keys, showcasing proficiency in front-end development and user interface design.",
+    image: work13,
+    live: "https://sohanr.github.io/Drum-kit/",
+    code: "https://github.com/SohanR/Drum-kit",
+    tag: ["VanillaJS"],
+  },
+  {
+    title: "Type Fast",
+    details:
+      "typing mini-game utilizing Vanilla JavaScript, where players are challenged to swiftly type displayed words for scores and time extension. Implemented features include difficulty level selection, dynamic time extension upon accurate word typing, and a game-over scenario upon time expiration.",
+    image: work14,
+    live: "https://sohanr.github.io/type-fast/",
+    code: "https://github.com/SohanR/type-fast",
+    tag: ["VanillaJS"],
+  },
+  {
+    title: "To Do",
+    details:
+      "A simple to do app with simple animation effect with feature of filtering, mark as done and deleting.",
+    image: work16,
+    live: "https://github.com/SohanR/todo-list",
+    code: "https://sohanr.github.io/todo-list/",
+    tag: ["VanillaJS"],
+  },
+];
 
 export const projectNav = [
-    {
-        "name": 'all'
-    },
+  {
+    name: "all",
+  },
 
-    {
-        "name": 'Full Stack'
-    },
+  {
+    name: "Full Stack",
+  },
 
-    {
-        "name": 'Frontend'
-    },
+  {
+    name: "Frontend",
+  },
 
-    {
-        "name": 'ReactJS'
-    },
-    
-    {
-        "name": 'NextJS'
-    },    
-    
-    {
-        "name": 'NestJS'
-    },
+  {
+    name: "ReactJS",
+  },
 
-    {
-        "name": 'ExpressJS'
-    },
+  {
+    name: "NextJS",
+  },
 
-    {
-        "name": 'React Native'
-    },
-    {
-        "name": 'VanillaJS'
-    },
-    {
-        "name": 'Leetcode'
-    },
-    {
-        "name": 'Misc'
-    },
-]
+  {
+    name: "NestJS",
+  },
+
+  {
+    name: "ExpressJS",
+  },
+
+  {
+    name: "React Native",
+  },
+  {
+    name: "VanillaJS",
+  },
+  {
+    name: "Leetcode",
+  },
+  {
+    name: "Misc",
+  },
+];
 
 export const contactData = {
-    "title": "Contact Me",
-    "subtitle":"Get in touch",
-    "subheading":"Talk to me",
-    "form":{
-        "title": "Contact with me anytime",
-        "name":"Enter your name",
-        "email":"Enter your email address",
-        "message":"Write your message",
-    },
-    "email":{
-        "title":"Email",
-        "subtitle":"Email Me",
-        "link":"sohans.bd@gmail.com"
-    },
+  title: "Contact Me",
+  subtitle: "Get in touch",
+  subheading: "Talk to me",
+  form: {
+    title: "Contact with me anytime",
+    name: "Enter your name",
+    email: "Enter your email address",
+    message: "Write your message",
+  },
+  email: {
+    title: "Email",
+    subtitle: "Email Me",
+    link: "sohans.bd@gmail.com",
+  },
 
-    "linkedin":{
-        "title":"Linkedin",
-        "subtitle":"Connect With Me",
-        "link":"linkedin.com/in/mizanurrahman/"
-    },
+  linkedin: {
+    title: "Linkedin",
+    subtitle: "Connect With Me",
+    link: "linkedin.com/in/mizanurrahman/",
+  },
 
-    "github":{
-        "title":"Github",
-        "subtitle":"Follow Me",
-        "link":"github.com/SohanR"
-    }
-}
+  github: {
+    title: "Github",
+    subtitle: "Follow Me",
+    link: "github.com/SohanR",
+  },
+};
